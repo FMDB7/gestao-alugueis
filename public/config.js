cd404bc2ab3580b20abe98528a8b3ea783e5f54c
FMDB7 <marquesfb7@gmail.com>
2026-07-13 16:41:39 -0400

    Delete public directory

--- public/config.js
+++
@@ -1,8 +0,0 @@
-const CONFIG = {
-  clientId: "b270c0cd-fc9a-4551-84f5-2fe3c6be60fa",
-  tenantId: "8003b84e-097a-420a-afa4-ca9eab1bc74d",
-  fileId: "947BE6F8-B5A9-47BA-889C-975B03BC4E15",
-  redirectUri: window.location.origin,
-  tableName: "BASE",
-  sheetName: "BASE"
-};
